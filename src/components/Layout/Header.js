8b63e0bb91b3cf04d4ec3a3a59fc6ac1654f9e72
Quonn S Bernard <quonn.bernard@gmail.com>
2022-06-26 13:33:48 -0400

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from "react"
+import mealsPic from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import { HeaderCartButton } from "./HeaderCartButton";
+
+export const Header = props => {
+    return <Fragment>
+                <header className={classes.header}>
+                    <h1>ReactMeals</h1>
+                    <HeaderCartButton></HeaderCartButton>
+                </header>
+                <div className={classes['main-image']}>
+                    <img src={mealsPic} alt='table full of food' />
+                </div>
+            </Fragment>
+}
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+
+import { Fragment } from "react";
+import { MealsSummary } from "./MealsSummary";
+import { AvailableMeals } from "./AvailableMeals";
+
+export const Meals = () => {
+    return <Fragment>
+        <MealsSummary />
+        <AvailableMeals />
+    </Fragment>
+}
